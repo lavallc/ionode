cae6e4bb6d139ea71422ddc70c461cbe887080a5
Eric Barch <eric.barch@lava.io>
2014-03-04 12:20:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ionode - ion lamp api
+===========================
+
+Control your Ion lamp from node!
---
+++ test.js
@@ -0,0 +1,35 @@
+var ionode = require('./lib/index'),
+    ion = ionode.createLamp('d99f89c9a3a9');
+
+
+
+ion.connect();
+
+
+var currentHue = 0;
+
+
+ion.on('ready', function() {
+	console.log('lamp ready');
+
+	// pulse pattern
+	ion.setPattern(5, function(err) {
+		if (!err) {
+			setHue();
+		}
+	});
+	
+});
+
+
+// begin hue infinite loop
+function setHue() {
+	ion.setPatternConfig(5, 1, currentHue, function(err) {
+		if (!err) {
+			setTimeout(setHue(), 1000);
+		}
+	});
+	currentHue++;
+	if (currentHue > 359)
+		currentHue = 0;
+}
